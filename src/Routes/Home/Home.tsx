b67ce9d183e3a7e2dfb09d184949e3ab923d4bdf
Josep Salvat Lopez <damaskinoss@gmail.com>
2021-04-07 18:18:54 +0200

    Initial commit + Eslint + Prettier Config

---
+++ src/Routes/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './home.module.scss';
+
+const Home: React.FC = () => {
+  return <div className={styles.homeContainer}>Home MI casa</div>;
+};
+
+export default Home;
